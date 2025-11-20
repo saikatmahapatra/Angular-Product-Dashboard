36c36aaef49dd8fdaa77cc0cb9350b9d179fc403
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2025-11-20 09:57:52 +0530

    Add routing

--- ui-client/product-dashboard/src/app/app.routes.ts
+++ ui-client/product-dashboard/src/app/app.routes.ts
@@ -15,6 +15,18 @@ export const routes: Routes = [
     loadComponent: () => import('./assignment/assignment.component').then(m => m.AssignmentComponent)
   },
   {
+    path: 'manage-products',
+    loadComponent: () => import('./manage-products/manage-products.component').then(m => m.ManageProductsComponent)
+  },
+  {
+    path: 'add-edit-product/:id',
+    loadComponent: () => import('./add-edit-products/add-edit-products.component').then(m => m.AddEditProductsComponent)
+  },
+  {
+    path: 'my-cart',
+    loadComponent: () => import('./my-cart/my-cart.component').then(m => m.MyCartComponent)
+  },
+  {
     path: '**',
     redirectTo: '/products'
   }
