24a083f1fea74341933a0239c8668a36d7c4c5e8
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2025-11-20 09:55:57 +0530

    create components

---
+++ ui-client/product-dashboard/src/app/add-edit-products/add-edit-products.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-products',
+  imports: [],
+  templateUrl: './add-edit-products.component.html',
+  styleUrl: './add-edit-products.component.scss'
+})
+export class AddEditProductsComponent {
+
+}
---
+++ ui-client/product-dashboard/src/app/my-cart/my-cart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-cart',
+  imports: [],
+  templateUrl: './my-cart.component.html',
+  styleUrl: './my-cart.component.scss'
+})
+export class MyCartComponent {
+
+}
