c813dfc5a0d4a3af032c51e1173a0ad478436b34
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2025-11-21 08:15:31 +0530

    Using reactive form

--- ui-client/product-dashboard/src/app/add-edit-products/add-edit-products.component.ts
+++ ui-client/product-dashboard/src/app/add-edit-products/add-edit-products.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { Category, CategoryService } from '../datasources/category.service';
-import { ProductService } from '../datasources/product.service';
+import { Product, ProductService } from '../datasources/product.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -21,14 +21,14 @@ import { MatButtonModule } from '@angular/material/button';
 
 
 export class AddEditProductsComponent implements OnInit {
-  addForm = {
-    title: '',
-    description: '',
-    category: 0,
-    price: '',
-    is_featured: false,
-    image_url: ''
-  }
+  addForm = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+    category: new FormControl<number | null>(null, [Validators.required, Validators.min(1)]),
+    price: new FormControl('', [Validators.required]),
+    is_featured: new FormControl(false),
+    image_url: new FormControl('', [Validators.required])
+  });
 
   categories: Category[] = [];
 
@@ -36,7 +36,8 @@ export class AddEditProductsComponent implements OnInit {
     private categoryService: CategoryService,
     private productService: ProductService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
@@ -59,7 +60,15 @@ export class AddEditProductsComponent implements OnInit {
 
   saveProduct(action: string) {
     if (action === 'add') {
-      this.productService.createProduct(this.addForm).subscribe({
+      const payload: Partial<Product> = {
+        title: this.addForm.value.title!,
+        description: this.addForm.value.description!,
+        category: this.addForm.value.category!,
+        price: this.addForm.value.price!,
+        is_featured: this.addForm.value.is_featured!,
+        image_url: this.addForm.value.image_url!
+      }
+      this.productService.createProduct(payload).subscribe({
         next: (response) => {
           this.goToProductList();
         },
